b03ab701fe8885ddf8084ba3fa0de3f2deab706c
JelledeBue <jelle@debue.nl>
2020-01-16 21:38:35 +0100

    Whoooo Update

--- New Unity Project (4)/Assets/Scripts/Scripts/Magnet.cs
+++ New Unity Project (4)/Assets/Scripts/Scripts/Magnet.cs
@@ -7,6 +7,7 @@ public class Magnet : Weapons
     public GameObject magneticField;
     public float magneticTimer;
     public Animator magnetAnimator;
+    public MagnetParticle magnetpull;
 
     public void Fire()
     {
@@ -17,12 +18,15 @@ public class Magnet : Weapons
     {
         magnetAnimator.Play("Folding In");
         GameManager.instance.soundMan.Play("Magnet");
+        magnetpull.PlayParticle();
+        
     }
 
     public void ResetWeapon()
     {
         magnetAnimator.Play("Folding Out");
         GameManager.instance.soundMan.Stop("Magnet");
+        magnetpull.StopParticle();
     }
 
     IEnumerator SetMagnet()
